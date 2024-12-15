34520a2a88ce4175a5ada5b8d3e921599f98224e
yogi-pradana <yogipradanaisdiyanto@mail.ac.id>
2024-12-16 00:01:04 +0700

    first

--- app/src/main/java/com/p3b1yogi/uas_p3b1_yogi/CalonUserData.kt
+++ app/src/main/java/com/p3b1yogi/uas_p3b1_yogi/CalonUserData.kt
@@ -3,7 +3,7 @@ package com.p3b1yogi.uas_p3b1_yogi
 // data class `CalonUserData` digunakan untuk merepresentasikan informasi film pada bagian pengguna (user)
 // setiap properti pada data class ini merepresentasikan atribut dari sebuah film, seperti judul, sutradara
 // durasi, rating, sinopsis, dan URL gambar
-// data class ini digunakan untuk menyimpan data film ketika diambil dari Firebase atau digunakan di bagian pengguna aplikasi
+// data class ini digunakan untuk menyimpan data calon ketika diambil dari Firebase atau digunakan di bagian pengguna aplikasi
 class CalonUserData(
     val name : String?= null,
     val age : String?= null,
